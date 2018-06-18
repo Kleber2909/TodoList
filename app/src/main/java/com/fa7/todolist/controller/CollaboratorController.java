32dbb14481d6aa8e6bd02ccc471141b9271780ef
Leandro Jackson <leandro-jpinh@hotmail.com>
2018-06-18 17:58:10 -0300

    Adicionando controllers iniciais.

---
+++ app/src/main/java/com/fa7/todolist/controller/CollaboratorController.java
@@ -0,0 +1,11 @@
+package com.fa7.todolist.controller;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+public class CollaboratorController extends ControllerBase {
+    
+    public CollaboratorController(Context context, AppCompatActivity appCompatActivity) {
+        super(context, appCompatActivity);
+    }
+}
